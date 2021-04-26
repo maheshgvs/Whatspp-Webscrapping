4e231e6f8fb16a3da80d2e6cbb9f8b59e3ada807
maheshgvs <82176510+maheshgvs@users.noreply.github.com>
2021-04-26 15:39:55 +0530

    Adding continuous integration to whatsapp web scrapping

--- README.md
+++ README.md
@@ -1,12 +1,5 @@
 # assignment
-prerequisite
-1.Install puppeteer
-"npm install puppeteer"
-
-Steps to run the Whatsapp.js program
-1. Clone the repo to VS code
-2. In Code line 13,14,15 mention the target whatsapp group name for web scrapping
-3.  Run the program using command "node.whatsapp.js"
-4.  once the whatsapp web launched, Scan the QR code from your whatsapp account
-5.  Target whatsapp group will be selected and browser will closed automatically
-6.  Results.json file will be generated and whatsapp group messages will store on this json file
+Steps to execute whatsapp webscrapping script
+1.Execute npm install in terminal
+2.Execute npm run start in terminal
+3. Scan QR code from mobile whatsapp
--- whatsapp.js
+++ whatsapp.js
@@ -1,53 +1,67 @@
 
-const puppeteer = require("puppeteer");
+const { ConsoleMessage } = require('playwright');
+const playwright = require('playwright');
 
-async function scrape (url){
+async function scrape(url) {
+    // Invoke the chromium browser and hot the desired web page
+    const browser = await playwright["chromium"].launch({ headless: false, args: ['--start-maximized'] });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto(url);
+    // Wait for the page to load and selcts the group name from whatsapp
+    await page.waitForSelector("#side > div.SgIJV > div > label > div > div._2_1wd.copyable-text.selectable-text");
+    const input = await page.$("#side > div.SgIJV > div > label > div > div._2_1wd.copyable-text.selectable-text")
+    const groupname = 'CAW_Assignment_Mahesh';
+    await input.type(groupname);
+    await page.waitForSelector("span[title='" + groupname + "']");
+    const target = await page.$("span[title='" + groupname + "']");
+    await target.click();
 
-    const browser = await puppeteer.launch({ headless: false ,
-        defaultViewport: null,
-        args: ['--start-maximized']});
-        const page = await browser.newPage();
-        await page.goto(url);
-        await page.waitForSelector("#side > div.SgIJV > div > label > div > div._2_1wd.copyable-text.selectable-text");
-        const input = await page.$("#side > div.SgIJV > div > label > div > div._2_1wd.copyable-text.selectable-text")
-        await input.type('test group');
-        await page.waitForSelector("span[title='Test Group']");
-        const target =await page.$("span[title='Test Group']");
-        await target.click(); 
-        await page.waitForSelector("._1bR5a");
-        await page.waitForSelector("._24wtQ._2kR4B");
-        const results = await page.evaluate(()=> {
-        const searchResult = document.querySelectorAll("._1bR5a");
-        
-        const temp=[];        
-        searchResult.forEach((searchItem)=> {
-        let str;
-        if (searchItem.querySelector("._1Lc2C.eHxwV._3-8er") !== null) {
-        str = searchItem.querySelector("._1Lc2C.eHxwV._3-8er").innerHTML;
-        console.log(str);
-        }
-        else if(searchItem.querySelector(".ZJv7X") !== null)
-        {
-            str = searchItem.querySelector(".ZJv7X").innerHTML;
-            console.log(str);
-        }
-        else {
-        str = "Me"; 
-        
-        }
-        let item={
+    await page.waitForSelector("._1bR5a");
+
+    // Scraping the new message from the whatsapp group
+    const reuse = async function () {
+        const results = await page.evaluate(() => {
+
+            const searchResult = document.querySelectorAll("._1bR5a");
+            const temp = [];
+
+            searchResult.forEach((searchItem) => {
+
+                let str;
+                const x = searchItem.querySelector("._1bR5a > div").getAttribute("data-pre-plain-text");
                 
-            contact : str,
-            Message: searchItem.querySelector("._3-8er.selectable-text.copyable-text>span").innerHTML,
-            Time: searchItem.querySelector("div._2zWo9 > div > span").innerHTML
-        };
-        temp.push(item);
-    });
-    return temp;
-    });
-    require("fs").writeFile("result.json",JSON.stringify(results,null,' '),() => {});
-    browser.close();
+                if (searchItem.querySelector("._1Lc2C.eHxwV._3-8er") !== null) {
+                    str = searchItem.querySelector("._1Lc2C.eHxwV._3-8er").innerHTML;
+                    console.log(str);
+                }
+                else {
+
+                    str = x;
+                }
+                let item = {
+
+                    contact: str,
+                    Message: searchItem.querySelector("._3-8er.selectable-text.copyable-text>span").innerHTML,
+                    Time: searchItem.querySelector("div._2zWo9 > div > span").innerHTML
+                };
+                temp.push(item);
+            });
+
+            return temp;
+        });
+
+        require("fs").writeFile("result.json", JSON.stringify(results, null, ' '), () => { });
+        console.log("New message being searched for every 10 Secounds.");
+    }
+
+
+    reuse();
+    try {
+        setInterval(reuse, 10000);
+    } catch (error) { console.log(error) };
 }
-    scrape("https://web.whatsapp.com/");
+
+scrape("https://web.whatsapp.com/");
 
 
